d18f05851efdee3c09d449c26d07bae90ea76204
Anton Grabovsky <agrabovsky2020@gmail.com>
2024-12-03 11:00:37 +0300

    Update conform

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,10 +1,7 @@
 local options = {
   formatters_by_ft = {
-    lua = { "stylua" },
-    javascript = { "biomejs" },
     php = { "php_cs_fixer" },
-    html = { "biomejs" },
-    css = { "biomejs" },
+    yaml = { "yamlfmt" },
   },
 
   format_on_save = {
