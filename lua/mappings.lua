67e9e545da9cd812003594b3e958f658f46900c3
Anton Grabovsky <agrabovsky2020@gmail.com>
2024-12-03 11:54:02 +0300

    Update mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -3,16 +3,32 @@ require "nvchad.mappings"
 local set = vim.keymap.set
 local del = vim.keymap.del
 
-set("n", "<leader>n", "<cmd>enew<CR>", { desc = "buffer new" })
+-- override NvChad
 del("n", "<leader>b")
-
-set("n", "<leader>c", function()
-  require("nvchad.tabufline").close_buffer()
-end, { desc = "buffer close" })
-del("n", "<leader>x")
+set("n", "<leader>n", "<cmd>enew<CR>", { desc = "buffer new" })
+del("n", "<leader>rn")
 del("n", "<leader>ch")
+del("n", "<leader>fm")
+set("n", "<C-\\>", function()
+  require("conform").format { lsp_fallback = true }
+end, { desc = "general format file" })
+del("n", "<leader>ds")
+set("n", "<leader>ld", vim.diagnostic.setloclist, { desc = "LSP diagnostic loclist" })
+del("n", "<leader>ma")
+set("n", "<leader>fm", "<cmd>Telescope marks<CR>", { desc = "telescope find marks" })
 del("n", "<leader>cm")
+set("n", "<leader>fc", "<cmd>Telescope git_commits<CR>", { desc = "telescope git commits" })
+del("n", "<leader>gt")
+set("n", "<leader>fs", "<cmd>Telescope git_status<CR>", { desc = "telescope git status" })
+del("n", "<leader>pt")
+del("n", "<leader>th")
+set("n", "<leader>ft", function()
+  require("nvchad.themes").open()
+end, { desc = "telescope nvchad themes" })
+set({ "n", "v" }, "<leader>h", "<cmd>NvCheatsheet<CR>", { desc = "mappings" })
+del("n", "<leader>v")
 
+-- close buffers
 set("n", "<leader>bc", function()
   require("nvchad.tabufline").closeAllBufs(false)
 end, { desc = "close all buffers except current" })
@@ -20,6 +36,18 @@ set("n", "<leader>bC", function()
   require("nvchad.tabufline").closeAllBufs()
 end, { desc = "close all buffers" })
 
+-- close windows
+set("n", "<leader>q", "<cmd>confirm q<CR>", { desc = "quit window" })
+set("n", "<leader>Q", "<cmd>confirm qall<CR>", { desc = "exit nvim" })
+
+-- split windows
+set("n", "\\", "<cmd>vsplit<CR>", { desc = "vertical split" })
+set("n", "|", "<cmd>split<CR>", { desc = "horizontal split" })
+
+-- Mason
+set("n", "<leader>p", "<cmd>Mason<CR>", { desc = "mason" })
+
+-- DAP
 set("n", "<leader>du", function()
   require("dapui").toggle()
 end, { desc = "toggle debug ui" })
@@ -33,36 +61,22 @@ set("n", "<leader>dn", function()
   require("dap").step_over()
 end, { desc = "debug step over" })
 
+-- LSP
 set("n", "gD", vim.lsp.buf.declaration, { desc = "go to declaration" })
 set("n", "gd", vim.lsp.buf.definition, { desc = "go to definition" })
 set("n", "gi", vim.lsp.buf.implementation, { desc = "go to implementation" })
-set("n", "<leader>sh", vim.lsp.buf.signature_help, { desc = "show signature help" })
-set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, { desc = "add workspace folder" })
-set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, { desc = "remove workspace folder" })
 set("n", "<leader>lr", vim.lsp.buf.references, { desc = "LSP references" })
 set({ "n", "v" }, "<leader>la", vim.lsp.buf.code_action, { desc = "LSP code action" })
 
+--  Git
 set("n", "<leader>gd", "<cmd>Gitsigns diffthis<CR>", { desc = "git diff" })
 set("n", "<leader>gl", "<cmd>Flog<CR>", { desc = "git log" })
 set("n", "<leader>gs", "<cmd>Floggit<CR>", { desc = "git status" })
 
-set("n", "<leader>q", "<cmd>confirm q<CR>", { desc = "quit window" })
-set("n", "<leader>Q", "<cmd>confirm qall<CR>", { desc = "exit nvim" })
-
-set("n", "\\", "<cmd>vsplit<CR>", { desc = "vertical split" })
-set("n", "|", "<cmd>split<CR>", { desc = "horizontal split" })
-
-del("n", "<leader>v")
-del("n", "<leader>rn")
-del("n", "<leader>pt")
-
-vim.keymap.set("n", "]t", function()
+-- Todo comments
+set("n", "]t", function()
   require("todo-comments").jump_next()
 end, { desc = "next todo comment" })
-vim.keymap.set("n", "[t", function()
+set("n", "[t", function()
   require("todo-comments").jump_prev()
 end, { desc = "previous todo comment" })
-
-set("n", "<leader>p", "<cmd>Mason<CR>", { desc = "mason" })
-
-set({ "n", "v" }, "<leader>h", "<cmd>NvCheatsheet<CR>", { desc = "mappings" })
