02a2ae6f8611d1074b7d83a3b5c3b2ab4c4b8527
Anton Grabovsky <agrabovsky2020@gmail.com>
2024-12-10 15:17:29 +0300

    Fix debug config for PHP

--- lua/configs/dap_config.lua
+++ lua/configs/dap_config.lua
@@ -1,79 +1,22 @@
 local ok, dap = pcall(require, "dap")
-local bin_path = vim.fn.stdpath "data" .. "/mason/bin/"
+local path_pkg = vim.fn.stdpath "data" .. "/mason/packages/"
 
-local ext = ""
 if os.getenv "OS" == "Windows_NT" then
-  bin_path = bin_path:gsub("\\", "/")
-  ext = ".cmd"
+  path_pkg = path_pkg:gsub("\\", "/")
 end
 
 if not ok then
   return
 end
 
-local js_ts_config = {
-  {
-    type = "pwa_node",
-    request = "launch",
-    name = "Launch JS/TS file",
-    program = "${file}",
-    cwd = "${workspaceFolder}",
-  },
-  {
-    type = "pwa_node",
-    request = "attach",
-    name = "Attach JS/TS",
-    processId = require("dap.utils").pick_process,
-    cwd = "${workspaceFolder}",
-  },
-}
-
 dap.configurations = {
-  javascript = js_ts_config,
-  typescript = js_ts_config,
-  php = {
-    {
-      name = "Launch built-in PHP server and debug",
-      type = "php",
-      request = "launch",
-      runtimeArgs = {
-        "-S",
-        "localhost:8000",
-        "-t",
-        ".",
-      },
-      port = 9003,
-      serverReadyAction = {
-        action = "openExternally",
-      },
-    },
-    {
-      name = "Debug current PHP script in console",
-      type = "php",
-      request = "launch",
-      program = "${file}",
-      cwd = "${fileDirname}",
-      externalConsole = false,
-      port = 9003,
-    },
-    {
-      name = "Listen for Xdebug",
-      type = "php",
-      request = "launch",
-      port = 9003,
-    },
-  },
+  php = {},
 }
 
 dap.adapters = {
-  pwa_node = {
-    type = "executable",
-    command = bin_path .. "js-debug-adapter" .. ext,
-    args = {},
-  },
   php = {
     type = "executable",
-    command = bin_path .. "php-debug-adapter" .. ext,
-    args = {},
+    command = "node",
+    args = { path_pkg .. "php-debug-adapter/extension/out/phpDebug.js" },
   },
 }
