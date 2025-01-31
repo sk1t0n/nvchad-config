e928af5d5a5cc70945cdf733e3cd1a75071d54d4
Anton Grabovsky <agrabovsky2020@gmail.com>
2025-01-31 19:59:01 +0300

    Update conform.lua

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,7 +1,7 @@
 local options = {
   formatters_by_ft = {
     php = { "pint", "php_cs_fixer", stop_after_first = true },
-    go = { "golines" },
+    go = { "gofmt", "goimports", "golines" },
     yaml = { "yamlfmt" },
   },
 
