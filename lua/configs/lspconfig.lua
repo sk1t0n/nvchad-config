6b6078f9f26e6f2b7a85b47ae904cc29820631e9
Anton Grabovsky <agrabovsky2020@gmail.com>
2025-02-09 11:26:27 +0300

    Update lspconfig.lua

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -29,7 +29,6 @@ local nvlsp = require "nvchad.configs.lspconfig"
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
     on_init = nvlsp.on_init,
     capabilities = nvlsp.capabilities,
   }
