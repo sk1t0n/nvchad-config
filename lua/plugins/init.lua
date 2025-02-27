2a7845fc9331075883f7a7395b7847ce6c604ee2
Anton Grabovsky <agrabovsky2020@gmail.com>
2025-02-27 17:51:02 +0300

    Update tiny-inline-diagnostic.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -218,7 +218,9 @@ return {
     event = "VeryLazy", -- Or `LspAttach`
     priority = 1000, -- needs to be loaded in first
     config = function()
-      require("tiny-inline-diagnostic").setup()
+      require("tiny-inline-diagnostic").setup {
+        preset = "powerline",
+      }
       vim.diagnostic.config { virtual_text = false } -- Only if needed in your configuration, if you already have native LSP diagnostics
     end,
   },
