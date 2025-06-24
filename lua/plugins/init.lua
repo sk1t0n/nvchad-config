29a64b239a90ee6837d898095b811e4c2fbea3a7
Anton Grabovsky <agrabovsky2020@gmail.com>
2025-06-24 11:48:43 +0300

    Add snippets for Go

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -35,7 +35,20 @@ return {
       require "configs.cmp"
     end,
   },
-  { "L3MON4D3/LuaSnip" },
+  {
+    "L3MON4D3/LuaSnip",
+    dependencies = {
+      {
+        "rafamadriz/friendly-snippets",
+        config = function()
+          require("luasnip.loaders.from_vscode").lazy_load()
+          require("luasnip.loaders.from_vscode").lazy_load {
+            paths = { vim.fn.stdpath "config" .. "/snippets" },
+          }
+        end,
+      },
+    },
+  },
   {
     "Exafunction/codeium.nvim",
     dependencies = {
